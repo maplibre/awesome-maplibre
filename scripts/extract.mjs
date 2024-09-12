1340d3623c752b7e000331aafd9e735c3fbe08fd
Hugues Tavernier <hugues.tavernier@protonmail.com>
2024-09-12 08:23:21 +0200

    script move banner from template file to script

--- scripts/extract.mjs
+++ scripts/extract.mjs
@@ -31,4 +31,7 @@ for (let [placeholder, identifier] of placeholderMatches) {
   templateContent = templateContent.replace(placeholder, groups[identifier]);
 }
 
-console.log(templateContent);
+const banner = `<!-- Do not edit. This file is generated from Awesome MapLibre -->
+<!-- https://github.com/maplibre/awesome-maplibre/tree/main/scripts -->\n\n`;
+
+console.log(banner + templateContent);
--- scripts/plugins.template.md
+++ scripts/plugins.template.md
@@ -1,6 +1,3 @@
-<!-- Do not edit. This file is generated from Awesome MapLibre -->
-<!-- https://github.com/maplibre/awesome-maplibre/tree/main/scripts -->
-
 # Plugins
 
 <!-- [JAVASCRIPT-PLUGINS] -->
